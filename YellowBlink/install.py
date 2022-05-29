61ce08b68b56ee2de2180cf83df2fabd7b0778a1
Olivier Devauchelle <olivier@leechong.ipgp.fr>
2022-05-29 19:25:48 +0200

    python env

--- YellowBlink/install.py
+++ YellowBlink/install.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Olivier Devauchelle, 2021
+# Olivier Devauchelle, 2022
 
 import os
 import sys
