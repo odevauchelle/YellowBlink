09569f937a23d3b182363a2e372b4ca3f1d1acd9
Olivier Devauchelle <olivier@leechong.ipgp.fr>
2023-06-24 15:52:16 +0200

    BBC stream url

--- YellowBlink/webradios.py
+++ YellowBlink/webradios.py
@@ -3,7 +3,7 @@
 
 webradios = {
     'fip' : dict( full_name = 'FIP', url = 'http://icecast.radiofrance.fr/fip-lofi.mp3' ),
-    'bbc4' : dict( full_name = 'BBC 4', url = 'http://stream.live.vc.bbcmedia.co.uk/bbc_radio_fourfm'),
+    'bbc' : dict( full_name = 'BBC', url = 'http://stream.live.vc.bbcmedia.co.uk/bbc_world_service'),
     'fr_cul' : dict( full_name = 'Fr. Culture', url = 'http://icecast.radiofrance.fr/franceculture-lofi.mp3'),
     'npr' : dict( full_name = 'NPR', url = 'http://npr-ice.streamguys1.com/live.mp3'),
      }
